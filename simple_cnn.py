97703b21afb4d1176f1edba745fad801cdae3743
Aureliano <54444048+Aurelianooo@users.noreply.github.com>
2025-12-06 10:45:59 +0800

    Add files via upload

---
+++ simple_cnn.py
@@ -0,0 +1,132 @@
+import os
+import torch
+import random
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+from torchinfo import summary
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+train_dataset = datasets.MNIST(root='./dataset', train=True, download=False, transform=transform)
+test_dataset = datasets.MNIST(root='./dataset', train=False, download=False, transform=transform)
+
+print("Length of train dataset: ", len(train_dataset))
+print("Length of test dataset: ", len(test_dataset))
+train_loader = DataLoader(dataset=train_dataset, batch_size=256, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=256, shuffle=False)
+fig = plt.figure()
+for i in range(12):
+    plt.subplot(3, 4, i + 1)
+    plt.tight_layout()
+    plt.imshow(train_dataset.data[i], cmap='gray', interpolation='none')
+    plt.title("Labels: {}".format(train_dataset.targets[i]))
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
+
+
+class MyClass(nn.Module):
+    def __init__(self):
+        super(MyClass, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(10, 20, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(320, 50),
+            nn.Linear(50, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.fc(x)
+        return x
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = MyClass().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# summary(model, (64, 1, 28, 28))
+
+
+def train(model, train_loader, optimizer, epoch):
+    model.train()
+    running_loss = 0.0
+    for i in range(epoch):
+        for batch_idx, data in enumerate(train_loader):
+            inputs, labels = data[0].to(device), data[1].to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if batch_idx % 100 == 0:
+                print(f'Epoch [{i + 1}/{epoch}], Step [{batch_idx}/{len(train_loader)}], Loss: {loss.item():.4f}')
+                running_loss = 0.0
+        if (i + 1) % 10 == 0:
+            torch.save(model.state_dict(), './checkpoints/model_{}.pth'.format(i))
+            print('Model saved as model_{}.pth'.format(i))
+
+
+train(model, train_loader, optimizer, 100)
+classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+# 1) 在 test 函数里收集
+y_trues, y_preds = [], []
+model.eval()
+with torch.no_grad():
+    for x, y in test_loader:
+        x = x.to(device)
+        logits = model(x)
+        preds = logits.argmax(dim=1).cpu()
+        y_preds.extend(preds.numpy())
+        y_trues.extend(y.numpy())
+
+# 2) 计算并打印报告
+print(classification_report(y_trues, y_preds, target_names=classes))
+
+# 3) 混淆矩阵
+cm = confusion_matrix(y_trues, y_preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                              display_labels=classes)
+disp.plot(cmap=plt.cm.Blues)
+plt.xticks(rotation=45)
+plt.title("Confusion Matrix")
+plt.show()
+
+
+def test(model, test_loader):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'Accuracy of the model on the test images: {100 * correct / total:.2f}%')
+
+
+test(model, test_loader)
